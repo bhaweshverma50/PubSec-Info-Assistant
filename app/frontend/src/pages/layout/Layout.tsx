2c96f07bda5a3f79710629e6eeb87ff6ba2a05e6
Bhawesh Kumar Verma <bhaweshk@terawe.com>
2024-06-03 19:31:23 +0530

    fix: logo updated

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import { Outlet, NavLink, Link } from "react-router-dom";
-import openai from "../../assets/new white logo.svg";
+import openai from "../../assets/vollee.png";
 import { WarningBanner } from "../../components/WarningBanner/WarningBanner";
 import styles from "./Layout.module.css";
 import { Title } from "../../components/Title/Title";
